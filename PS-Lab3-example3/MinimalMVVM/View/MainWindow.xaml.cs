19fcfe513659209fa6c72de835d8a329582fb50c
YovoManolov <yovo.manolov@abv.bg>
2018-05-12 20:28:34 +0300

    update 12.05.2018
    update 12.05.2018

---
+++ PS-Lab3-example3/MinimalMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MinimalMVVM.ViewModel;
+
+namespace MinimalMVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PresenterToLC psToLC = new PresenterToLC();
+        Presenter ps = new Presenter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = psToLC;
+        }
+
+        private void ChangeDC(object sender, RoutedEventArgs e)
+        {
+            if(this.DataContext is PresenterToLC)
+            {
+                this.DataContext = ps;
+            }
+            else 
+            {
+                this.DataContext = psToLC;
+            }
+        }
+    }
+}
