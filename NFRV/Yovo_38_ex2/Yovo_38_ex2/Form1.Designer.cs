557ad20da58c9d4278ba4e3132269e44e9b9684d
YovoManolov <yovo.manolov@abv.bg>
2018-11-10 15:46:55 +0200

    CR_NFRV first commit 10.11.2018
    CR_NFRV first commit 10.11.2018

---
+++ NFRV/Yovo_38_ex2/Yovo_38_ex2/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Yovo_38_ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+       
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PB_Image = new System.Windows.Forms.PictureBox();
+            this.loadImageId = new System.Windows.Forms.Button();
+            this.patterningId = new System.Windows.Forms.Button();
+            this.Quantization = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_Image
+            // 
+            this.PB_Image.Location = new System.Drawing.Point(12, 3);
+            this.PB_Image.Name = "PB_Image";
+            this.PB_Image.Size = new System.Drawing.Size(783, 375);
+            this.PB_Image.TabIndex = 0;
+            this.PB_Image.TabStop = false;
+            // 
+            // loadImageId
+            // 
+            this.loadImageId.Location = new System.Drawing.Point(42, 396);
+            this.loadImageId.Name = "loadImageId";
+            this.loadImageId.Size = new System.Drawing.Size(140, 42);
+            this.loadImageId.TabIndex = 1;
+            this.loadImageId.Text = "Load Image";
+            this.loadImageId.UseVisualStyleBackColor = true;
+            this.loadImageId.Click += new System.EventHandler(this.OnLoadImage);
+            // 
+            // patterningId
+            // 
+            this.patterningId.Location = new System.Drawing.Point(272, 396);
+            this.patterningId.Name = "patterningId";
+            this.patterningId.Size = new System.Drawing.Size(140, 42);
+            this.patterningId.TabIndex = 2;
+            this.patterningId.Text = "Patterning";
+            this.patterningId.UseVisualStyleBackColor = true;
+            this.patterningId.Click += new System.EventHandler(this.OnPatterningProcess);
+            // 
+            // Quantization
+            // 
+            this.Quantization.Location = new System.Drawing.Point(507, 396);
+            this.Quantization.Name = "Quantization";
+            this.Quantization.Size = new System.Drawing.Size(140, 42);
+            this.Quantization.TabIndex = 3;
+            this.Quantization.Text = "Quantization";
+            this.Quantization.UseVisualStyleBackColor = true;
+            this.Quantization.Click += new System.EventHandler(this.OnQuantizationProcess);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Quantization);
+            this.Controls.Add(this.patterningId);
+            this.Controls.Add(this.loadImageId);
+            this.Controls.Add(this.PB_Image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_Image;
+        private System.Windows.Forms.Button loadImageId;
+        private System.Windows.Forms.Button patterningId;
+        private System.Windows.Forms.Button Quantization;
+    }
+}
+
---
+++ NFRV/Yovo_38_ex2/Yovo_38_ex2/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yovo_38_ex2
+{
+    public partial class Form1 : Form
+    {
+        ImageProcessor imageProcessor;
+        public Form1()
+        {
+            InitializeComponent();
+            imageProcessor = new ImageProcessor();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ApplyImage(bool setSize = false)
+        {
+            if (setSize)
+                PB_Image.Size = new Size(imageProcessor.Width, imageProcessor.Height);
+            PB_Image.Image = imageProcessor.Bitmap;
+            
+        }
+
+        private void OnLoadImage(object sender, EventArgs e)
+        {
+            using (var fileDialg = new OpenFileDialog())
+            {
+                if (fileDialg.ShowDialog() == DialogResult.OK)
+                {
+                    imageProcessor.LoadImageData(fileDialg.FileName);
+                    ApplyImage(true);
+                }
+            }
+        }
+
+        private void OnQuantizationProcess(object sender, EventArgs e)
+        {
+            imageProcessor.QuantizationProcess();
+            ApplyImage();
+        }
+
+        private void OnPatterningProcess(object sender, EventArgs e)
+        {
+            imageProcessor.PatterningProcess();
+            ApplyImage();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                components?.Dispose();
+
+            }
+            base.Dispose(disposing);
+        }
+
+        class ImageProcessor : IDisposable
+        {
+            private String fileName;
+            public Bitmap Bitmap { get; private set; }
+            public int Width
+            {
+                get
+                {
+                    Debug.Assert(this.Bitmap != null);
+                    return this.Bitmap.Width;
+                }
+            }
+            public int Height
+            {
+                get
+                {
+                    Debug.Assert(this.Bitmap != null);
+                    return this.Bitmap.Height;
+                }
+            }
+
+            private const int byteSize = 255;
+            private const int matrixN = 2;
+            private const double step = byteSize / 4.0;
+
+            internal void LoadImageData(string fName)
+            {
+                try
+                {
+                    if (this.Bitmap != null) this.Bitmap.Dispose();
+                    this.Bitmap = new Bitmap(this.fileName = fName);
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+
+            internal void PatterningProcess()
+            {
+                double sum;
+                for (int i = 0; i < this.Width; i = i + matrixN)
+                {
+                    for (int j = 0; j < this.Height; j = j + matrixN)
+                    {
+                        sum = 0;
+                        int pixelCount = 0;
+                        for (int k = i; k < i + matrixN; k++)
+                            for (int m = j; m < j + matrixN; m++)
+                            {
+                                if (k >= this.Width || m >= this.Height) continue;
+                                sum += this.Bitmap.GetPixel(k, m).R;
+                                pixelCount++;
+                            }
+                        if (pixelCount > 0)
+                                    sum /= pixelCount;
+                        var colorMatrix = sum == byteSize ?
+                            new Color[,] { { Color.White, Color.White }, { Color.White, Color.White } } :
+                                                sum >= 3 * step && sum < byteSize ?
+                            new Color[,] { { Color.White, Color.Black }, { Color.White, Color.White } } :
+                                                sum >= 2 * step && sum < 3 * step ?
+                            new Color[,] { { Color.White, Color.Black }, { Color.Black, Color.White } } :
+                                                sum >= step && sum < 2 * step ?
+                            new Color[,] { { Color.White, Color.Black }, { Color.Black, Color.Black } } :
+                                                sum >= 0.0 && sum < step ?
+                            new Color[,] { { Color.Black, Color.Black }, { Color.Black, Color.Black } } :
+                                                null;
+
+                        Debug.Assert(colorMatrix != null);
+                        FillMatrix(i, j, colorMatrix);
+                    }
+                }
+            }
+
+            private void FillMatrix(int wIndex, int hIndex, Color[,] colors)
+            {
+                this.Bitmap.SetPixel(wIndex, hIndex, colors[0, 0]);
+                if (hIndex + 1 < this.Height)
+                    this.Bitmap.SetPixel(wIndex, hIndex + 1, colors[0, 1]);
+                if (wIndex + 1 < this.Width)
+                    this.Bitmap.SetPixel(wIndex + 1, hIndex, colors[1, 0]);
+                if (wIndex + 1 < this.Width && hIndex + 1 < this.Height)
+                    this.Bitmap.SetPixel(wIndex +1, hIndex + 1, colors[1, 1]);
+            }
+
+            internal void QuantizationProcess()
+            {
+                for (int i = 0; i < this.Width; i++)
+                {
+                    for (int j = 0; j < this.Height; j++)
+                    {
+                        if (this.Bitmap.GetPixel(i, j).R < 127)
+                        {
+                            this.Bitmap.SetPixel(i, j, Color.Black);
+                        }
+                        else
+                        {
+                            this.Bitmap.SetPixel(i, j, Color.White);
+                        }
+                    }
+                }
+            }
+
+            bool disposed;
+            public void Dispose()
+            {
+                if (disposed) return;
+                disposed = true;
+                this.Bitmap?.Dispose();
+                this.fileName = null;
+            }
+        }
+    }
+}
